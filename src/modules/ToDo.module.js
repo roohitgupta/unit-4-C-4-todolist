f41d5cb1c76d36be7d355c65a68558d3328582ca
roohitgupta <roohitgupta2@gmail.com>
2022-03-28 10:48:58 +0530

    id mentioned

--- src/modules/ToDo.module.js
+++ src/modules/ToDo.module.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose")
 const ToDoSchema = new mongoose.Schema({
     title: {type:String, required: true},
 
+    userID : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    }
 },
 {
     versionKey: false,
